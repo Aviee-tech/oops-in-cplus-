0f7cbeb0cc7d2ea665716d57f732eaaeed45efce
Aviee-tech <avisheksah678@gmail.com>
2026-02-04 18:58:47 +0530

    starting c++ programs

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //library
+using namespace std; // standard library 
+// means like we dont have to do like std::cout 
+// instead of that we can directly do like cout directly
+int main()
+{
+    int a=10;
+    cout<<a;
+    return 0;
+}
